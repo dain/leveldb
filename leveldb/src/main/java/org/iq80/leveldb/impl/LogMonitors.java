1aa3156b3a7354b15e44cd9798f179ebce99b6cd
Hiram Chirino <hiram@hiramchirino.com>
2011-12-14 15:04:43 -0500

    Missing \n in console logging when corruption events occur.

--- leveldb/src/main/java/org/iq80/leveldb/impl/LogMonitors.java
+++ leveldb/src/main/java/org/iq80/leveldb/impl/LogMonitors.java
@@ -43,13 +43,13 @@ public final class LogMonitors
             @Override
             public void corruption(long bytes, String reason)
             {
-                System.out.print(String.format("corruption of %s bytes: %s", bytes, reason));
+                System.out.println(String.format("corruption of %s bytes: %s", bytes, reason));
             }
 
             @Override
             public void corruption(long bytes, Throwable reason)
             {
-                System.out.print(String.format("corruption of %s bytes", bytes));
+                System.out.println(String.format("corruption of %s bytes", bytes));
                 reason.printStackTrace();
             }
         };
