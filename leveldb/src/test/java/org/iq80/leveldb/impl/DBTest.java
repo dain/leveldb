6240ce3fb92b45479c622a4e760b2511fe15ced5
Dan Frankowski <dan@code42.com>
2012-12-17 14:29:32 -0600

    Simple test: add, delete, check.

---
+++ leveldb/src/test/java/org/iq80/leveldb/impl/DBTest.java
@@ -0,0 +1,55 @@
+package org.iq80.leveldb.impl;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.iq80.leveldb.DB;
+import org.iq80.leveldb.DBIterator;
+import org.iq80.leveldb.Options;
+import org.iq80.leveldb.WriteOptions;
+import org.iq80.leveldb.util.FileUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DBTest {
+    private File databaseDir;
+
+    @BeforeMethod
+    public void setUp()
+            throws Exception
+    {
+        databaseDir = FileUtils.createTempDir("leveldb");
+    }
+
+    @AfterMethod
+    public void tearDown()
+            throws Exception
+    {
+        FileUtils.deleteRecursively(databaseDir);
+    }
+
+    @Test
+    void test1() throws IOException {
+        DB db = Iq80DBFactory.factory.open(databaseDir, new Options());
+
+        // Add an element
+        byte[] ONE = new byte[] { 1 };
+        WriteOptions writeOptions = new WriteOptions();
+        writeOptions.sync(true);
+        db.put(ONE, ONE, writeOptions);
+
+        // Delete it
+        db.delete(ONE, writeOptions);
+
+        // Test that it's gone
+        DBIterator iter = db.iterator();
+        long size = 0;
+        while (iter.hasNext()) {
+            iter.next();
+            size++;
+        }
+        Assert.assertEquals(size, 0);
+    }
+}
