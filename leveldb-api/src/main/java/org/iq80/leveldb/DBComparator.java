84c1c97590e1d95769616e1912f4cb2bc6497f6a
David Phillips <david@acz.org>
2017-12-18 11:54:32 -0800

    Fix building Javadoc

--- leveldb-api/src/main/java/org/iq80/leveldb/DBComparator.java
+++ leveldb-api/src/main/java/org/iq80/leveldb/DBComparator.java
@@ -28,13 +28,13 @@ public interface DBComparator
     String name();
 
     /**
-     * If <code>start < limit</code>, returns a short key in [start,limit).
+     * If {@code start < limit}, returns a short key in [start,limit).
      * Simple comparator implementations should return start unchanged,
      */
     byte[] findShortestSeparator(byte[] start, byte[] limit);
 
     /**
-     * returns a 'short key' where the 'short key' >= key.
+     * returns a 'short key' where the 'short key' is greater than or equal to key.
      * Simple comparator implementations should return key unchanged,
      */
     byte[] findShortSuccessor(byte[] key);
